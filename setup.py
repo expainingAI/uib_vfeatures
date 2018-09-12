d1b846241d2c504b55e29e4379b2a0bd4ae14fd1
Miquel Miró Nicolau <miquelca32@gmail.com>
2018-09-12 12:59:04 +0200

    Added the installation instructions

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='uib_vfeatures',
       version='0.1',
       description='Vision features of generalistic use',
-      url='http://github.com/storborg/funniest',
+      url='https://gitlab.com/miquelca32/features',
       author='UIB-UGIVIA',
-      author_email='miquel.miro1@estudiants.uib.cat',
+      author_email=['miquelca32@gmail.com', 'bernat_galmes@hotmail.com', 'gabriel_moya@uib.es'],
       license='MIT',
       packages=['uib_vfeatures'],
       install_requires=[
