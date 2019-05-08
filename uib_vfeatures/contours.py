added elongation, min and max feret calculation, fixed min_r calculation, changed method names feret and breadth to major&minor axes, added few private helper methods

uib_vfeatures/contours.py
@@ -1,5 +1,6 @@
 import cv2
 import math
+import numpy as np
 
 
 class Contours:
@@ -70,7 +71,7 @@ class Contours:
         :param contour:
         :return:
         """
-        return Contours.feret(contour) * Contours.breadth(contour)
+        return Contours.major_axis(contour) * Contours.minor_axis(contour)
 
     @staticmethod
     def rectangularity(contour):
@@ -98,39 +99,47 @@ class Contours:
     @staticmethod
     def min_r(contour):
         """
-        @brief Calculates the minor radius of the ellipse from the contour
+        @brief Calculates the radius of the maximum inscribed circle of the contour
 
         :param contour:
         :return:
         """
-        return Contours.breadth(contour) / 2
+        _, _, width, height = cv2.boundingRect(contour)
+        width = int(width) * 2
+        height = int(height) * 2
+        raw_distance = np.empty((width, height), dtype=np.float32)
+        for i in range(width):
+            for j in range(height):
+                raw_distance[i, j] = cv2.pointPolygonTest(contour, (j, i), True)
+        _, maxVal, _, _ = cv2.minMaxLoc(raw_distance)
+        return abs(maxVal)
 
     @staticmethod
-    def feret(contour):
+    def major_axis(contour):
         """
-        @brief Calculates the major diagonal of the enclosing ellipse of the contour
+        @brief Calculates the major axis of the enclosing ellipse of the contour
 
         :param contour:
         :return:
         """
-        (_, _), (major, _), _ = cv2.fitEllipse(contour)
+        (_, _), (_, major), _ = cv2.fitEllipse(contour)
         return major
 
     @staticmethod
-    def breadth(contour):
+    def minor_axis(contour):
         """
-        @brief Calculates the minor diagonal of the ellipse from the contour
+        @brief Calculates the minor axis of the ellipse from the contour
 
         :param contour:
         :return:
         """
-        (_, _), (_, minor), _ = cv2.fitEllipse(contour)
+        (_, _), (minor, _), _ = cv2.fitEllipse(contour)
         return minor
 
     @staticmethod
-    def feret_angle(contour):
+    def orientation(contour):
         """
-        @brief Find the angle between the feret and the horizontal
+        @brief Find the angle orientation of the enclosing ellipse
 
         :param contour:
         :return:
@@ -148,7 +157,7 @@ class Contours:
         :param contour: 1 channel image
         :return:
         """
-        return round(4 * Contours.area(contour) / (math.pi * Contours.feret(contour) * Contours.feret(contour)), 2)
+        return round(4 * Contours.area(contour) / (math.pi * Contours.major_axis(contour) * Contours.major_axis(contour)), 2)
 
     @staticmethod
     def circularity(contour):
@@ -191,7 +200,7 @@ class Contours:
         :return:
         """
 
-        return round(Contours.feret(contour) / Contours.breadth(contour), 2)
+        return round(Contours.major_axis(contour) / Contours.minor_axis(contour), 2)
 
     @staticmethod
     def area_equivalent_diameter(contour):
@@ -219,7 +228,7 @@ class Contours:
         :param contour:
         :return:
         """
-        return (math.pi * Contours.feret(contour) * Contours.breadth(contour)) / 4
+        return math.pi * Contours.major_axis(contour) * Contours.minor_axis(contour)
 
     @staticmethod
     def compactness(contour):
@@ -229,7 +238,7 @@ class Contours:
         :param contour:
         :return:
         """
-        return math.sqrt((4 / math.pi * Contours.area(contour)) / Contours.feret(contour))
+        return math.sqrt((4 / math.pi * Contours.area(contour)) / Contours.major_axis(contour))
 
     @staticmethod
     def convexity(contour):
@@ -254,7 +263,7 @@ class Contours:
 
     @staticmethod
     def r_factor(contour):
-        return Contours.convex_hull_perimeter(contour) / (Contours.feret(contour) * math.pi)
+        return Contours.convex_hull_perimeter(contour) / (Contours.major_axis(contour) * math.pi)
 
     @staticmethod
     def eccentricity(contour):
@@ -273,6 +282,35 @@ class Contours:
         d = math.fabs(ellipse[0][1] - ellipse[1][1])
 
         return round(D / d, 2)
+
+    @staticmethod
+    def max_feret(contour):
+        """
+        The maximum distance between parallel tangents to the projection area of the contour
+        :param contour:
+        :return:
+        """
+        feret, _ = Contours._max_min_feret(contour)
+        return feret
+
+    @staticmethod
+    def min_feret(contour):
+        """
+        The minimum distance between parallel tangents to the projection area of the contour
+        :param contour:
+        :return:
+        """
+        _, feret = Contours._max_min_feret(contour)
+        return feret
+
+    @staticmethod
+    def elongation(contour):
+        """
+        Relation between maximum and minimum feret of the contour.
+        :param contour:
+        :return:
+        """
+        return Contours.max_feret(contour) / Contours.min_feret(contour)
 
     @staticmethod
     def hu_moments(contour):
@@ -294,3 +332,92 @@ class Contours:
         cy = int(m['m01'] / m['m00'])
 
         return (cx, cy)
+
+    @staticmethod
+    def _max_min_feret(contour):
+        """
+        Helper method for calculation of maximum and minimum ferets based on convex hull of the contour.
+        Based on C++ code: https://www.crisluengo.net/archives/408
+        :param contour:
+        :return:
+        """
+        convex_hull_contour = Contours.convex_hull(contour)
+        min_feret = 999999
+        max_feret = 0
+        n = len(convex_hull_contour) - 1
+        p0 = n
+        p = 0
+        q = 1
+
+        while Contours._triangle_area(convex_hull_contour[p][0], convex_hull_contour[next(p, n)][0],
+                            convex_hull_contour[next(q, n)][0]) > Contours._triangle_area(convex_hull_contour[p][0],
+                                                                                convex_hull_contour[next(p, n)][0],
+                                                                                convex_hull_contour[q][0]):
+            q = next(q, n)
+
+        while p != p0:
+            p = next(p, n)
+            listq = [q]
+            while Contours._triangle_area(convex_hull_contour[p][0], convex_hull_contour[next(p, n)][0],
+                                convex_hull_contour[next(q, n)][0]) > Contours._triangle_area(convex_hull_contour[p][0],
+                                                                                    convex_hull_contour[next(p, n)][0],
+                                                                                    convex_hull_contour[q][0]):
+                q = next(q, n)
+                listq.append(q)
+
+            if Contours._triangle_area(convex_hull_contour[p][0], convex_hull_contour[next(p, n)][0],
+                             convex_hull_contour[next(q, n)][0]) == Contours._triangle_area(convex_hull_contour[p][0],
+                                                                                  convex_hull_contour[next(p, n)][0],
+                                                                                  convex_hull_contour[q][0]):
+                listq.append(next(q, n))
+
+            for i in range(len(listq)):
+                q = ((listq[i] - 1) % n) + 1
+                d = math.sqrt((convex_hull_contour[p][0][0] - convex_hull_contour[q][0][0]) ** 2 +
+                              (convex_hull_contour[p][0][1] - convex_hull_contour[q][0][1]) ** 2)
+                if d > max_feret:
+                    max_feret = d
+
+            p3 = convex_hull_contour[p][0]
+            for i in range(len(listq) - 2):
+                p1 = convex_hull_contour[listq[i]][0]
+                p2 = convex_hull_contour[listq[i + 1]][0]
+                height = Contours._triangle_height(p1, p2, p3)
+
+                if height < min_feret:
+                    min_feret = height
+
+        return max_feret, min_feret
+
+    @staticmethod
+    def _triangle_area(p1, p2, p3):
+        """
+        Helper method that calculates triangle area based on triangle vertices
+        :param p1: First vertex of the triangle
+        :param p2: Second vertex of the triangle
+        :param p3: Third vertex of the triangle
+        :return:
+        """
+        return ((p2[0] - p1[0]) * (p3[1] - p1[1]) - (p2[1] - p1[1]) * (p3[0] - p1[0])) / 2
+
+    @staticmethod
+    def _triangle_height(p1, p2, p3):
+        """
+        Helper method that calculates triangle height based on triangle vertices
+        :param p1: First vertex of the triangle
+        :param p2: Second vertex of the triangle
+        :param p3: Third vertex of the triangle
+        :return:
+        """
+        return ((p2[0] - p1[0]) * (p3[1] - p1[1]) - (p2[1] - p1[1]) * (p3[0] - p1[0])) / \
+               math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+
+    @staticmethod
+    def _next(p, n):
+        """
+        Helper method that calculates next antipodal point
+        :param p: previous point
+        :param n: total number of points in a contour
+        :return:
+        """
+        return p % n + 1